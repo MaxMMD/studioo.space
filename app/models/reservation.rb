b1a2c372c6905b5582b385874e3a717f7a4ac48c
Pedro Cobos <pedro.cobos@live.com>
2020-02-27 12:03:41 -0600

    Added validations and removed unnecessary links in spaces#show

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,8 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :space
 
+  validates :start_on, :end_on, presence: true
+
   def number_of_days
     (end_on - start_on).to_i
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :encrypted_password, presence: true
 end
