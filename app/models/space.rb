fcceea190b8d68b4926801c6f852ecf3908c8e42
Max Dupont <maxime.maletdupont@gmail.com>
2020-02-27 20:04:27 -0500

    done

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,8 +3,13 @@ class Space < ApplicationRecord
 
   has_many :reservations, dependent: :destroy
   belongs_to :user
-  geocoded_by :address
+
+  geocoded_by :full_address
   after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, :address, :price_per_day, :city, :content, presence: true
+
+  def full_address
+    "#{address} - #{city}"
+  end
 end
