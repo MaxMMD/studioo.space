6c5bb13a3a3863da89eb7879a0dc362e55ca4660
Max Dupont <maxime.maletdupont@gmail.com>
2020-03-09 20:52:48 -0400

    almost there

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_184503) do
+ActiveRecord::Schema.define(version: 2020_03_09_235209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_184503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "reservation_id"
+    t.integer "ratings"
     t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
   end
 
