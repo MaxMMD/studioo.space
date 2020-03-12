0d5a4ff44dc7ab5364b16ecf5f94b71215a6cf77
Jose Carlos Juarez <josecarlos.juarez@gmail.com>
2020-03-12 10:17:45 -0600

    Devise fix for class

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,13 @@ class User < ApplicationRecord
   def renter?
     spaces.any?
   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
